16818324db1292888640d9b9ad99f6fa57936164
yekim0325 <yeeun940325@gmail.com>
2024-03-17 16:49:02 +0900

    React 개념정리(컴포넌트 작성,  css 작성법, 이벤트 처리, state, props 등)

--- voca/src/App.js
+++ voca/src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
 import Hello from './component/Hello';
 import Welcome from './component/Welcome';
+import styles from "./App.module.css";
 
 function App() {
-  const name = "Tom";
-  const naver = {
-    name : '네이버' ,
-    url : 'https://naver.com' ,
-  };
+  // const name = "Tom";
+  // const naver = {
+  //   name : '네이버' ,
+  //   url : 'https://naver.com' ,
+  // };
   return (
     <div className="App">
-      <Hello />
-      <Hello />
-      <Hello />
-      <Welcome />
+      <h3>props : properties</h3>
+      <Hello age={10}/>
+      <Hello age={20}/>
+      <Hello age={30}/>
+      {/* <div className={styles.box}>App</div> */}
     </div>
   );
 }
--- voca/src/component/Hello.js
+++ voca/src/component/Hello.js
@@ -5,15 +5,71 @@
 
 // export default Hello;
 
-import World from './World';
+// import World from './World';
+// import styles from "./Hello.module.css";
 
 // 방법2.
-export default function Hello(){
+// export default function Hello(){
+//   function showName(){
+//     console.log("Mike");
+//   }
+
+//   function showAge(age){
+//     console.log(age);
+//   }
+  
+//   function showText(txt){
+//     console.log(txt);
+//   }
+
+//   return (
+//     <div>
+//       <h1>Hello</h1>
+//       <button onClick={showName}>Show name</button>
+//       <button
+//         onClick={() => {
+//           showAge(30);
+//         }}
+//       >
+//         Show age
+//       </button>
+//       {/* <input type="text" onChange={showText} /> */}
+//       <input type="text" onChange={(e)=>{
+//         //console.log(e.target.value);
+//         const txt = e.target.value;
+//         showText(txt);
+//       }} />
+//     </div>
+//   );
+// }
+import {useState} from "react";
+import UserName from "./UserName";
+
+export default function Hello({age}){
+  // let name = "Mike"
+  const [name, setName] = useState('Mike');
+  const msg = age > 19 ? "성인 입니다" : "미성년자 입니다.";
+
+  function chageName(){
+    // const newName = name === 'Mike' ? 'Jane' : 'Mike';
+    // console.log(name);
+    // document.getElementById("name").innerText = name;
+    setName(name === 'Mike' ? 'Jane' : 'Mike');
+  }
+
   return (
     <div>
-      <h1>Hello</h1>
-      <World/>
-      <World/>
+      <h2 id="name">
+        {name}({age} : {msg})
+      </h2>
+      <UserName name={name}/>
+      <button
+        onClick={() => {
+          setName(name === 'Mike' ? 'Jane' : 'Mike');
+        }}
+      >
+        Change
+      </button>
     </div>
   );
 }
